1f189bc7e7a307cf279a1fc6bf03faecc289daf7
Andras Lasso <lasso@queensu.ca>
2024-08-19 01:26:13 +0200

    Updated version to 1.1.0

--- slicerio/_version.py
+++ slicerio/_version.py
@@ -1,7 +1,7 @@
 import re
 from typing import Tuple
 
-__version__: str = '1.0.0'
+__version__: str = '1.1.0'
 
 __version_info__: Tuple[str, str, str] = tuple(
     re.match(r'(\d+\.\d+\.\d+).*', __version__).group(1).split('.')
